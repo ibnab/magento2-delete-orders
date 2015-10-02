d19fc87189bcdc6a6231690825548069b96d3024
said elmarrouani <contact@ibnab.com>
2015-10-02 13:05:03 +0100

    first commit

---
+++ Controller/Adminhtml/Order/MassDelete.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Ibnab\DeleteOrders\Controller\Adminhtml\Order;
+use Magento\Framework\Model\Resource\Db\Collection\AbstractCollection;
+use Magento\Backend\App\Action\Context;
+class MassDelete extends \Magento\Sales\Controller\Adminhtml\Order\AbstractMassAction
+{
+  public $_resource;
+
+  public function __construct(Context $context,
+\Magento\Framework\App\Resource $resource)
+  {
+    $this->_resource = $resource;
+    parent::__construct($context);
+ 
+  }
+    /**
+     * Cancel selected orders
+     *
+     * @param AbstractCollection $collection
+     * @return \Magento\Backend\Model\View\Result\Redirect
+     */
+    protected function massAction(AbstractCollection $collection)
+    {
+        $countCancelOrder = 0;
+        $connection = $this->_resource->getConnection(\Magento\Framework\App\Resource::DEFAULT_READ_RESOURCE);
+        $connectionWriter  = $this->_resource->getConnection(\Magento\Framework\App\Resource::DEFAULT_WRITE_RESOURCE);
+        $showTables = $connectionWriter->fetchCol('show tables');
+//start get table name
+        
+        $tblSalesOrder = $connection->getTableName('sales_order');
+        $tblSalesCreditmemoComment = $connection->getTableName('sales_creditmemo_comment');
+        $tblSalesCreditmemoItem = $connection->getTableName('sales_creditmemo_item');
+        $tblSalesCreditmemo = $connection->getTableName('sales_creditmemo');
+        $tblSalesCreditmemoGrid = $connection->getTableName('sales_creditmemo_grid');
+        $tblSalesInvoiceComment = $connection->getTableName('sales_invoice_comment');
+        $tblSalesInvoiceItem = $connection->getTableName('sales_invoice_item');
+        $tblSalesInvoice = $connection->getTableName('sales_invoice');
+        $tblSalesInvoiceGrid = $connection->getTableName('sales_invoice_grid');
+        $tblQuoteAddressItem = $connection->getTableName('quote_address_item');
+        $tblQuoteItemOption = $connection->getTableName('quote_item_option');
+        $tblQuote = $connection->getTableName('quote');
+        $tblQuoteAddress = $connection->getTableName('quote_address');
+        $tblQuoteItem = $connection->getTableName('quote_item');
+        $tblQuotePayment = $connection->getTableName('quote_payment');
+        $tblQuoteShippingRate = $connection->getTableName('quote_shipping_rate');
+        $tblQuoteIDMask = $connection->getTableName('quote_id_mask');
+        $tblSalesShipmentComment = $connection->getTableName('sales_shipment_comment');
+        $tblSalesShipmentItem = $connection->getTableName('sales_shipment_item');
+        $tblSalesShipmentTrack = $connection->getTableName('sales_shipment_track');
+        $tblSalesShipment = $connection->getTableName('sales_shipment');
+        $tblSalesShipmentGrid = $connection->getTableName('sales_shipment_grid');
+        $tblSalesOrderAddress = $connection->getTableName('sales_order_address');
+        $tblSalesOrderItem = $connection->getTableName('sales_order_item');
+        $tblSalesOrderPayment = $connection->getTableName('sales_order_payment');
+        $tblSalesOrderStatusHistory = $connection->getTableName('sales_order_status_history');
+        $tblSalesOrderGrid = $connection->getTableName('sales_order_grid');
+        $tblLogQuote = $connection->getTableName('log_quote');
+        $showTablesLog = $connectionWriter->fetchCol('SHOW TABLES LIKE ?', '%'.$tblLogQuote);
+        $tblSalesOrderTax = $connection->getTableName('sales_order_tax');       
+        foreach ($collection->getItems() as $order) {
+
+                $orderId = $order->getId();
+                if ($order->getIncrementId()) {
+                    $incId = $order->getIncrementId();
+                    if (in_array($tblSalesOrder, $showTables)) {
+                        $result1 = $connectionWriter->fetchAll('SELECT quote_id FROM `'.$tblSalesOrder.'` WHERE entity_id='.$orderId);
+                        $quoteId = (int) $result1[0]['quote_id'];
+                    }
+                    $connectionWriter->rawQuery('SET FOREIGN_KEY_CHECKS=1');
+                    if (in_array($tblSalesCreditmemoComment, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesCreditmemoComment.'` WHERE parent_id IN (SELECT entity_id FROM `'.$tblSalesCreditmemo.'` WHERE order_id='.$orderId.')');
+                    }
+                    if (in_array('sales__creditmemo_item', $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesCreditmemoItem.'` WHERE parent_id IN (SELECT entity_id FROM `'.$tblSalesCreditmemo.'` WHERE order_id='.$orderId.')');
+                    }
+                    if (in_array($tblSalesCreditmemo, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesCreditmemo.'` WHERE order_id='.$orderId);
+                    }
+                    if (in_array($tblSalesCreditmemoGrid, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesCreditmemoGrid.'` WHERE order_id='.$orderId);
+                    }
+                    if (in_array($tblSalesInvoiceComment, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesInvoiceComment.'` WHERE parent_id IN (SELECT entity_id FROM `'.$tblSalesInvoice.'` WHERE order_id='.$orderId.')');
+                    }
+                    if (in_array($tblSalesInvoiceItem, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesInvoiceItem.'` WHERE parent_id IN (SELECT entity_id FROM `'.$tblSalesInvoice.'` WHERE order_id='.$orderId.')');
+                    }
+                    if (in_array($tblSalesInvoice, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesInvoice.'` WHERE order_id='.$orderId);
+                    }
+                    if (in_array($tblSalesInvoiceGrid, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesInvoiceGrid.'` WHERE order_id='.$orderId);
+                    }
+                    if ($quoteId) {
+                        if (in_array($tblQuoteAddressItem, $showTables)) {
+                            $connectionWriter->rawQuery('DELETE FROM `'.$tblQuoteAddressItem.'` WHERE parent_item_id IN (SELECT address_id FROM `'.$tblQuoteAddress.'` WHERE quote_id='.$quoteId.')');
+                        }
+                        if (in_array($tblQuoteShippingRate, $showTables)) {
+                            $connectionWriter->rawQuery('DELETE FROM `'.$tblQuoteShippingRate.'` WHERE address_id IN (SELECT address_id FROM `'.$tblQuoteAddress.'` WHERE quote_id='.$quoteId.')');
+                        }
+                       if (in_array($tblQuoteIDMask, $showTables)) {
+                           $connectionWriter->rawQuery('DELETE FROM `'.$tblQuoteIDMask.'` where quote_id='.$quoteId);
+                        }
+                        if (in_array($tblQuoteItemOption, $showTables)) {
+                            $connectionWriter->rawQuery('DELETE FROM `'.$tblQuoteItemOption.'` WHERE item_id IN (SELECT item_id FROM `'.$tblQuoteItem.'` WHERE quote_id='.$quoteId.')');
+                        }
+                        if (in_array($tblQuote, $showTables)) {
+                            $connectionWriter->rawQuery('DELETE FROM `'.$tblQuote.'` WHERE entity_id='.$quoteId);
+                        }
+                        if (in_array($tblQuoteAddress, $showTables)) {
+                            $connectionWriter->rawQuery('DELETE FROM `'.$tblQuoteAddress.'` WHERE quote_id='.$quoteId);
+                        }
+                        if (in_array($tblQuoteItem, $showTables)) {
+                            $connectionWriter->rawQuery('DELETE FROM `'.$tblQuoteItem.'` WHERE quote_id='.$quoteId);
+                        }
+                        if (in_array('sales__quotePayment', $showTables)) {
+                            $connectionWriter->rawQuery('DELETE FROM `'.$tblQuotePayment.'` WHERE quote_id='.$quoteId);
+                        }
+                    }
+                    if (in_array($tblSalesShipmentComment, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesShipmentComment.'` WHERE parent_id IN (SELECT entity_id FROM `'.$tblSalesShipment.'` WHERE order_id='.$orderId.')');
+                    }
+                    if (in_array($tblSalesShipmentItem, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesShipmentItem.'` WHERE parent_id IN (SELECT entity_id FROM `'.$tblSalesShipment.'` WHERE order_id='.$orderId.')');
+                    }
+                    if (in_array($tblSalesShipmentTrack, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesShipmentTrack.'` WHERE order_id IN (SELECT entity_id FROM `'.$tblSalesShipment.'` WHERE parent_id='.$orderId.')');
+                    }
+                    if (in_array($tblSalesShipment, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesShipment.'` WHERE order_id='.$orderId);
+                    }
+                    if (in_array($tblSalesShipmentGrid, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesShipmentGrid.'` WHERE order_id='.$orderId);
+                    }
+                    if (in_array($tblSalesOrder, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesOrder.'` WHERE entity_id='.$orderId);
+                    }
+                    if (in_array($tblSalesOrderAddress, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesOrderAddress.'` WHERE parent_id='.$orderId);
+                    }
+                    if (in_array($tblSalesOrderItem, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesOrderItem.'` WHERE order_id='.$orderId);
+                    }
+                    if (in_array($tblSalesOrderPayment, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesOrderPayment.'` WHERE parent_id='.$orderId);
+                    }
+                    if (in_array($tblSalesOrderStatusHistory, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesOrderStatusHistory.'` WHERE parent_id='.$orderId);
+                    }
+                    if ($incId && in_array($tblSalesOrderGrid, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesOrderGrid.'` WHERE increment_id='.$incId);
+                    }
+                    if (in_array($tblSalesOrderTax, $showTables)) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblSalesOrderTax.'` WHERE order_id='.$orderId);
+                    }
+                    if ($quoteId && $showTablesLog) {
+                        $connectionWriter->rawQuery('DELETE FROM `'.$tblLogQuote.'` WHERE quote_id='.$quoteId);
+                    }
+                    $connectionWriter->rawQuery('SET FOREIGN_KEY_CHECKS=1');
+                }
+
+            $countCancelOrder++;
+        }
+        $countNonCancelOrder = $collection->count() - $countCancelOrder;
+
+        if ($countNonCancelOrder && $countCancelOrder) {
+            $this->messageManager->addError(__('%1 order(s) cannot be deleted.', $countNonCancelOrder));
+        } elseif ($countNonCancelOrder) {
+            $this->messageManager->addError(__('You cannot delete the order(s).'));
+        }
+
+        if ($countCancelOrder) {
+            $this->messageManager->addSuccess(__('We deleted %1 order(s).', $countCancelOrder));
+        }
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $resultRedirect->setPath('sales/*/');
+        return $resultRedirect;
+    }
+}
